a25afef2dc393fe6eb1cd13346887c5258265997
weby82 <damien.bouvier@gmail.com>
2016-12-19 18:54:26 +0100

    bootstrap
    ajout de bootstrap et jquery

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 <head>
 <meta charset="utf-8">
 	<title>Santon Elo</title>
-	<link rel="stylesheet" type="text/css" href="./style/main.css">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
+	 <!-- Bootstrap -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="./js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 <header>
