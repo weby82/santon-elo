ce1529fac098468bf1dec6176a2e9cf10a359b54
weby82 <damien.bouvier@gmail.com>
2017-01-28 16:08:23 +0100

    structure des dossiers

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<meta charset="utf-8">
-	<title>Santon Elo</title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="./css/main.css">
-	 <!-- Bootstrap -->
-    <link href="./css/bootstrap.min.css" rel="stylesheet">
-     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="./js/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-</head>
-<body>
-<header>
-	<h1>Santon Elo</h1>
-</header>
+<?php
+    require_once ("./private/view/header.php");
+?>
+    <main>
+        
+    </main>
+    <footer>
+
+    </footer>
+    <script type="text/javascript" src="./assets/js/main.js"></script>
 </body>
 </html>
