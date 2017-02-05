f817d9a5efde6e5e563b00766a7405123428c1dd
weby82 <damien.bouvier@gmail.com>
2017-02-05 18:34:30 +0100

    rajout page de liste de santon

---
+++ categorie-santons.php
@@ -0,0 +1,7 @@
+
+<?php
+    require_once ("./private/view/header.php");
+    require_once ("./private/view/section-categorie.php");
+    require_once ("./private/view/footer.php");
+
+?>
---
+++ private/view/section-categorie.php
@@ -0,0 +1,179 @@
+	<main>
+		<div class="container">
+			<div class="container-inner">
+				<h2>Nativité</h2>
+				<aside class="col-md-3 col-sm-12 col-nav-left">
+					<nav>
+						<h3>Catégories</h3></h3>
+						<ul class="nav nav-pills nav-stacked">
+							<li class="active">
+								<a href="#">Nativité</a>
+							</li>
+							<li class="">
+								<a href="#">Baptême</a>
+							</li>
+							<li class="">
+								<a href="#">Anniversaire</a>
+							</li>
+							<li class="">
+								<a href="#">Communion</a>
+							</li>
+							<li class="">
+								<a href="#">Mariage</a>
+							</li>
+							<li class="">
+								<a href="#">Commande spéciale</a>
+							</li>
+						</ul>
+					</nav>
+				</aside>
+				<section class="col-md-9 col-sm-12" id="section-liste">
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/mouton.jpg" alt="santon mouton">
+							</a>
+							<h3><a href="#" title="">Mouton</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/vache.jpg" alt="santon vache">
+							</a>
+							<h3><a href="#" title="">Vache</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/santon3.jpg" alt="santon 3">
+							</a>
+							<h3><a href="#" title="">Santon 3</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/ange.jpg" alt="santon ange">
+							</a>
+							<h3><a href="#" title="">Ange</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/mouton.jpg" alt="santon mouton">
+							</a>
+							<h3><a href="#" title="">Mouton</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/vache.jpg" alt="santon vache">
+							</a>
+							<h3><a href="#" title="">Vache</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/santon3.jpg" alt="santon 3">
+							</a>
+							<h3><a href="#" title="">Santon 3</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/ange.jpg" alt="santon ange">
+							</a>
+							<h3><a href="#" title="">Ange</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/mouton.jpg" alt="santon mouton">
+							</a>
+							<h3><a href="#" title="">Mouton</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/vache.jpg" alt="santon vache">
+							</a>
+							<h3><a href="#" title="">Vache</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/santon3.jpg" alt="santon 3">
+							</a>
+							<h3><a href="#" title="">Santon 3</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 bloc-santon">
+						<div class="bloc-santon-inner">
+							<a href="" title="">
+								<img src="./assets/img/santons/nativite/ange.jpg" alt="santon ange">
+							</a>
+							<h3><a href="#" title="">Ange</a></h3>
+							<p class="prix-santon">12€</p>
+							<a href="#" title="" class="ajout-panier btn btn-default">
+								Ajouter au panier
+							</a>
+						</div>
+					</div>
+				</section>
+			</div>
+		</div>	
+	
+	</main>
