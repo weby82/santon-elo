9823bd1364d2140654ab54f49c93fca03f76551d
LiinaaP <peeraully.liinaa@gmail.com>
2017-02-28 14:01:35 +0100

    ajout css liinaa

--- private/view/header.php
+++ private/view/header.php
@@ -12,6 +12,7 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="./assets/js/bootstrap.min.js"></script>
 
+    <link rel="stylesheet" type="text/css" href="./assets/css/liinaa.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
 </head>
 <body>
