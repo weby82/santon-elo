fa8ffd70b01bf33f1fee4a5541305787dcd56b8f
Skred04 <pier.philippon@gmail.com>
2017-03-01 16:57:55 +0100

    Modif Header
    Modif header (dropdown)

--- private/view/header.php
+++ private/view/header.php
@@ -27,6 +27,21 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
                   
 
+            <div class="dropdown navbar-right">
+              <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                <a id="panier" href="panier.php">Panier</a>
+                <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </div>
+
+
             <form class="navbar-form navbar-right" role="search">
               <div class="input-group">
                 <input type="text" class="form-control" placeholder="Rechercher">
@@ -41,34 +56,48 @@
         
         <nav class="navbar navbar-default">
             <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-                    </div>
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                      <ul class="nav nav-justified">
+              <!-- Brand and toggle get grouped for better mobile display -->
+                  <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                  </div>
+                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav nav-justified">
 
-                        <li class="active">
-                            <a href="index.php">Accueil</a>
-                        </li>
-                       
-                        <li><a href="#">Catégorie</a></li>
-                        <li><a href="#">Actualités</a></li>
+                          <li class="active">
+                              <a href="index.php">Accueil</a>
+                          </li>
+                          <li>
+                              <div id="dropcategorie" class="dropdown">
+                                <a href="#" class="dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                  Catégories
+                                  <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                                  <li><a href="#">Noël/Natavité</a></li>
+                                  <li><a href="#">Baptême</a></li>
+                                  <li><a href="#">Anniversaire</a></li>
+                                  <li><a href="#">Communion</a></li>
+                                  <li><a href="#">Mariage</a></li>
+                                  <li><a href="#">Commande spéciale</a></li>
+                                </ul>
+                              </div>
+                          </li> 
+                          <li><a href="#">Actualités</a></li>
 
-                        <li>
-                            <a href="connexion.php">Evènements</a>
-                        </li>
-                        <li>
-                            <a href="inscription.php">Livre d'or</a>
-                        </li>
-                        <li>
-                            <a href="contact.php">Contact</a>
-                        </li>
+                          <li>
+                              <a href="">Evènements</a>
+                          </li>
+                          <li>
+                              <a href="">Livre d'or</a>
+                          </li>
+                          <li>
+                              <a href="contact.php">Contact</a>
+                          </li>
                       </ul>
                     </div>
                 </div>
