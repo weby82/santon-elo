f50411aacaf7ae28ed969d41b4a58b9aaea6b5dc
KellyLau <kellylauger@hotmail.fr>
2017-03-02 17:10:41 +0100

    modif liste article
    modification responsive et style de la page article.php

--- private/view/section-liste-article.php
+++ private/view/section-liste-article.php
@@ -1,89 +1,82 @@
 
-    <h1>Actualité</h1>
-    <div class="trait"></div>
-    <section class="list-article">
-        <article>
-            <div class="list-article-inner">
-                <figure class="col-md-4">
-                    <img src="http://lorempixel.com/150/150/abstract/6/" alt="image 1" class="img-responsive">
-                </figure>
-                <div class="col-md-8 div-article">
-                    <h2>Titre de l'article</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <a class="readmore" href="">Lire la suite</a>
-                </div>
-            </div>
-        </article>
+   <main>
+        <div class="container">
+            <div class="container-inner">
+                <h2>Actualité</h2>
+                <section class="list-article">
+                    <article>
+                        <div class="list-article-inner">
+                            <figure class="col-md-4">
+                                <img src="http://lorempixel.com/150/150/abstract/6/" alt="image 1" class="img-responsive">
+                            </figure>
+                            <div class="col-md-8 div-article">
+                                <h3>Titre de l'article</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <a class="readmore" href="">Lire la suite</a>
+                            </div>
+                        </div>
+                    </article>
+                
+                    <article>
+                        <div class="list-article-inner">
+                            <figure class="col-md-4">
+                                <img src="http://lorempixel.com/150/150/abstract/6/" alt="image 2" class="img-responsive">
+                            </figure>
+                            <div class="col-md-8 div-article">
+                                <h3>Titre de l'article</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <a class="readmore" href="">Lire la suite</a>
+                            </div>
+                        </div>
+                    </article>
+                
+                    <article>
+                        <div class="list-article-inner">
+                            <figure class="col-md-4">
+                              <img src="http://lorempixel.com/150/150/abstract/6/" alt="imag 3" class="img-responsive">
+                            </figure>
+                            <div class="col-md-8 div-article">
+                                <h3>Titre de l'article</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <a class="readmore" href="">Lire la suite</a>
+                            </div>
+                        </div>
+                    </article>
+               
+                    <article>
+                        <div class="list-article-inner">
+                            <figure class="col-md-4">
+                                <img src="http://lorempixel.com/150/150/abstract/6/" alt="image-responsive 4" class="img-responsive">
+                            </figure>
+                            <div class="col-md-8 div-article">
+                                <h3>Titre de l'article</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <a class="readmore" href="">Lire la suite</a>
+                            </div>
+                        </div>
+                    </article>
+                </section>
     
-        <div class="clearfix">
-            &nbsp;
+            </div>
         </div>
-   
-        <article>
-            <div class="list-article-inner">
-                <figure class="col-md-4">
-                    <img src="http://lorempixel.com/150/150/abstract/6/" alt="image 2" class="img-responsive">
-                </figure>
-                <div class="col-md-8 div-article">
-                    <h2>Titre de l'article</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <a class="readmore" href="">Lire la suite</a>
-                </div>
-            </div>
-        </article>
-    
-        <div class="clearfix">
-            &nbsp;
-        </div>
-    
-        <article>
-            <div class="list-article-inner">
-                <figure class="col-md-4">
-                  <img src="http://lorempixel.com/150/150/abstract/6/" alt="imag 3" class="img-responsive">
-                </figure>
-                <div class="col-md-8 div-article">
-                    <h2>Titre de l'article</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <a class="readmore" href="">Lire la suite</a>
-                </div>
-            </div>
-        </article>
-    
-        <div class="clearfix">
-            &nbsp;
-        </div>
-   
-        <article>
-            <div class="list-article-inner">
-                <figure class="col-md-4">
-                    <img src="http://lorempixel.com/150/150/abstract/6/" alt="image-responsive 4" class="img-responsive">
-                </figure>
-                <div class="col-md-8 div-article">
-                    <h2>Titre de l'article</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <a class="readmore" href="">Lire la suite</a>
-                </div>
-            </div>
-        </article>
-    </section>
-    
+    </main>
