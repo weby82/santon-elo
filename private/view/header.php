8e1b1e165ad40ec41a128df98d10cb458109384d
weby82 <damien.bouvier@gmail.com>
2017-03-04 18:55:28 +0100

    style du menu

--- private/view/header.php
+++ private/view/header.php
@@ -66,70 +66,72 @@ require_once("private/database.php");
             </form>
         </div>
         
-        <nav class="navbar navbar-default">
-            <div class="container">
-              <!-- Brand and toggle get grouped for better mobile display -->
-                  <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                    </button>
-                  </div>
-                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav nav-justified">
+        
+    </header>
 
-                          <li <?php if($navActive == "accueil") { echo "class='active'"; }?>>
-                              <a href="index.php">Accueil</a>
-                          </li>
-                          <li <?php if($navActive == "categorie") { echo "class='active'"; }?>>
-                              <div id="dropcategorie" class="dropdown">
-                                <a href="#" class="dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                  Catégories
-                                  <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                                    <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "nativite") { echo "class='active'"; }?>>
-                                        <a href="./categorie-santons.php?categorie=nativite">Noël/Natavité</a>
-                                    </li>
-                                    <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "bapteme") { echo "class='active'"; }?>>
-                                        <a href="./categorie-santons.php?categorie=bapteme">Baptême</a>
-                                    </li>
-                                    <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "anniversaire") { echo "class='active'"; }?>>
-                                        <a href="./categorie-santons.php?categorie=anniversaire">Anniversaire</a>
-                                    </li>
-                                    <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "communion") { echo "class='active'"; }?>>
-                                        <a href="./categorie-santons.php?categorie=communion">Communion</a>
-                                    </li>
-                                    <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "mariage") { echo "class='active'"; }?>>
-                                        <a href="./categorie-santons.php?categorie=mariage">Mariage</a>
-                                    </li>
-                                    <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "speciale") { echo "class='active'"; }?>>
-                                        <a href="./categorie-santons.php?categorie=speciale">Commande spéciale</a>
-                                    </li>
-                                </ul>
-                              </div>
-                          </li> 
-                          <li <?php if($navActive == "actualites") { echo "class='active'"; }?>>
-                            <a href="actualites.php">Actualités</a>
-                          </li>
-                          <li <?php if($navActive == "evenements") { echo "class='active'"; }?>>
-                              <a href="">Evènements</a>
-                          </li>
-                          <li <?php if($navActive == "livre") { echo "class='active'"; }?>>
-                              <a href="">Livre d'or</a>
-                          </li>
-                          <li <?php if($navActive == "contact") { echo "class='active'"; }?>>
-                              <a href="contact.php">Contact</a>
-                          </li>
-                      </ul>
-                    </div>
+    <nav class="navbar navbar-default">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+              </div>
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav nav-justified">
+
+                      <li <?php if($navActive == "accueil") { echo "class='active'"; }?>>
+                          <a href="index.php">Accueil</a>
+                      </li>
+                      <li <?php if($navActive == "categorie") { echo "class='active'"; }?>>
+                          <div id="dropcategorie" class="dropdown">
+                            <a href="#" class="dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                              Catégories
+                              <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                                <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "nativite") { echo "class='active'"; }?>>
+                                    <a href="./categorie-santons.php?categorie=nativite">Noël/Natavité</a>
+                                </li>
+                                <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "bapteme") { echo "class='active'"; }?>>
+                                    <a href="./categorie-santons.php?categorie=bapteme">Baptême</a>
+                                </li>
+                                <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "anniversaire") { echo "class='active'"; }?>>
+                                    <a href="./categorie-santons.php?categorie=anniversaire">Anniversaire</a>
+                                </li>
+                                <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "communion") { echo "class='active'"; }?>>
+                                    <a href="./categorie-santons.php?categorie=communion">Communion</a>
+                                </li>
+                                <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "mariage") { echo "class='active'"; }?>>
+                                    <a href="./categorie-santons.php?categorie=mariage">Mariage</a>
+                                </li>
+                                <li <?php if($navActive == "categorie" && $_REQUEST["categorie"] == "speciale") { echo "class='active'"; }?>>
+                                    <a href="./categorie-santons.php?categorie=speciale">Commande spéciale</a>
+                                </li>
+                            </ul>
+                          </div>
+                      </li> 
+                      <li <?php if($navActive == "actualites") { echo "class='active'"; }?>>
+                        <a href="actualites.php">Actualités</a>
+                      </li>
+                      <li <?php if($navActive == "evenements") { echo "class='active'"; }?>>
+                          <a href="">Evènements</a>
+                      </li>
+                      <li <?php if($navActive == "livre") { echo "class='active'"; }?>>
+                          <a href="">Livre d'or</a>
+                      </li>
+                      <li <?php if($navActive == "contact") { echo "class='active'"; }?>>
+                          <a href="contact.php">Contact</a>
+                      </li>
+                  </ul>
                 </div>
             </div>
-        </nav>
+        </div>
+    </nav>
   
-    </header>
  <!-- Holds shopping cart info with selected items -->
     <div class="shopping_cart_holder">
         <a href="#" class="close_shopping_cart_holder" ><span class='glyphicon glyphicon-remove' ></span></a>
