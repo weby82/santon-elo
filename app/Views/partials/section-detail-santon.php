d661f22289dc34424b9739f9cc293163d744d2d0
weby82 <damien.bouvier@gmail.com>
2017-03-29 14:29:34 +0200

    lien photo

--- app/Views/partials/section-detail-santon.php
+++ app/Views/partials/section-detail-santon.php
@@ -66,6 +66,7 @@ if(!empty($tabLigne)){
 	$prixSanton						= $tabLigne["prix"];
 	$photoSanton					= $tabLigne["photo"];
 
+	$urlPhoto			= $this->assetUrl($photoSanton);
 
 		
 }
@@ -73,7 +74,7 @@ if(!empty($tabLigne)){
 ?>
 				<section class="col-md-9 col-sm-12 section-content">
 					<figure class="col-md-4 col-sm-3 col-xs-12">
-						<img src="<?php echo $photoSanton ?>">
+						<img src="<?php echo $urlPhoto ?>">
 					</figure>
 					<div class="col-md-8 col-sm-9 col-xs-12 detail-santon">
 						<h3><?php echo $nomSanton ?></h3>
