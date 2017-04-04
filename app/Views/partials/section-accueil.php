3214a6eb1ef281ec1811d3834d2985653f8fb687
KellyLau <kellylauger@hotmail.fr>
2017-04-04 13:59:28 +0200

    style img

--- app/Views/partials/section-accueil.php
+++ app/Views/partials/section-accueil.php
@@ -27,7 +27,9 @@ foreach ($tabLigneActu as $index => $tabColonneActu) {
 ?>
 				<article class="col-md-4 col-sm-6 col-xs-12 articles-actu">
 					<div class="article-inner">
-						<img src="<?php echo $urlPhoto ?>" alt="#" class="img-circle">
+						<figure>
+						<img src="<?php echo $urlPhoto ?>" alt="#">
+						</figure>
 						<div class="desc-actu">
 							<h3><?php echo $titreActu ?></h3>
 							<p><?php echo $contenuActu ?></p>
