0d2f4c3e3a5907da882f8f6527ecfe9f725800e1
weby82 <damien.bouvier@gmail.com>
2017-04-05 14:00:26 +0200

    retouche style bulle image detail santon

--- app/Views/partials/section-detail-santon.php
+++ app/Views/partials/section-detail-santon.php
@@ -73,9 +73,11 @@ if(!empty($tabLigne)){
 	// Construire le code HTML
 ?>
 				<section class="col-md-9 col-sm-12 section-content">
-					<figure class="col-md-4 col-sm-3 col-xs-12">
-						<img src="<?php echo $urlPhoto ?>" class="img-detail-santon">
-					</figure>
+					<div class="img-bulle col-md-4 col-sm-3 col-xs-12">
+						<figure>
+							<img src="<?php echo $urlPhoto ?>" class="img-detail-santon">
+						</figure>
+					</div>
 					<div class="col-md-8 col-sm-9 col-xs-12 detail-santon">
 						<h3><?php echo $nomSanton ?></h3>
 						<div id="description">
